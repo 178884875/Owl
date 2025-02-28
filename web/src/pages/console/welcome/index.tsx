896fef13703e8235990744a2f7cce2a67786f6df
token <239573049@qq.com>
2025-03-01 02:48:35 +0800

    Update thinking protocol and enhance ConsoleWelcome component with Chinese localization

--- web/src/pages/console/welcome/index.tsx
+++ web/src/pages/console/welcome/index.tsx
@@ -1,8 +1,18 @@
+import { Button, } from 'antd';
+import { Rocket, } from 'lucide-react';
+import './styles.css';
 
 export default function ConsoleWelcome() {
     return (
-        <div>
-            Welcome to Console
+        <div className="welcome-container">
+            <div className="welcome-header">
+                <Rocket className="welcome-icon" />
+                <h1>欢迎来到控制台</h1>
+            </div>
+            <p>这里是您的控制台，您可以在这里管理所有设置。</p>
+            <Button type="primary" className="start-button">
+                开始使用
+            </Button>
         </div>
     )
 }
